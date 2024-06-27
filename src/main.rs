dacaee0c9683dd0346424bd714a5f5c78afb0788
Ahmed Farghal <me@asoli.dev>
2024-06-27 20:54:23 +0100

    generic over return type

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
-fn pick<T>(value: u8, even: T, odd: T) -> T {
-    if value % 2 == 0 {
-        even
-    } else {
-        odd
+trait Random {
+    fn generate() -> Self;
+}
+
+impl Random for u8 {
+    fn generate() -> Self {
+        55
     }
 }
 
-fn main() {
-    let v = rand::random();
+impl Random for &str {
+    fn generate() -> Self {
+        "hola"
+    }
+}
 
-    println!("You picked {}, it's {}", v, pick(v, "even", "odd"));
-    println!("You picked {}, it's {}", v, pick(v, true, false));
-    println!("You picked {}, it's {}", v, pick(v, 100, 200));
+fn random<T>() -> T
+where
+    T: Random,
+{
+    T::generate()
+}
+
+fn pick(v: &str) {
+    println!("You picked {}", v);
+}
+
+fn main() {
+    //let v = rand::random();
+
+    let v = random();
+    pick(v);
 }
