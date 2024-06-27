75f53e8b8ea3098b8041e658e70e19d010472fe7
Ahmed Farghal <me@asoli.dev>
2024-06-27 21:49:45 +0100

    named lifetimes

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,48 @@
-trait Random {
-    fn generate() -> Self;
+#[derive(Debug)]
+enum Either<'a, 'b> {
+    This(&'a str),
+    That(&'b str),
 }
 
-impl Random for u8 {
-    fn generate() -> Self {
-        55
+#[derive(Clone)]
+struct Employee {
+    name: String,
+    age: u32,
+}
+
+fn get_name<'a>(employee_1: &'a Employee) -> &'a str {
+    &employee_1.name
+}
+
+fn who_is_older<'a, 'b>(employee_1: &'a Employee, employee_2: &'b Employee) -> Either<'a, 'b> {
+    if employee_1.age > employee_2.age {
+        Either::This(&employee_1.name)
+    } else {
+        Either::That(&employee_2.name)
     }
 }
-
-impl Random for &str {
-    fn generate() -> Self {
-        "hola"
-    }
-}
-
-fn random<T>() -> T
-where
-    T: Random,
-{
-    T::generate()
-}
-
-fn pick(v: &str) {
-    println!("You picked {}", v);
-}
+// fn who_is_older<'a>(employee_1: &'a Employee, employee_2: &'a Employee) -> &'a str {
+//     if employee_1.age > employee_2.age {
+//         &employee_1.name
+//     } else {
+//         &employee_2.name
+//     }
+// }
 
 fn main() {
-    //let v = rand::random();
+    let mahmoud = Employee {
+        name: "Mahmoud".to_string(),
+        age: 22,
+    };
 
-    let v = random();
-    pick(v);
+    let name = get_name(&mahmoud);
+
+    println!("My name is {}", name);
+
+    let ahmed = Employee {
+        name: "Ahmed".to_string(),
+        age: 20,
+    };
+
+    println!("The older is {:?}", who_is_older(&mahmoud, &ahmed));
 }
