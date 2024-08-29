ae1e4fd9e359c275a23134da7af61a93a3beb2c2
Ahmed Farghal <me@asoli.dev>
2024-08-29 21:39:04 +0100

    Example threading and sockets

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,67 @@
-use rust_for_everyone::{Amount, Currency};
-
-#[derive(Debug, Clone, Copy)]
-pub struct USD;
-
-impl Currency for USD {
-    const CODE: &'static str = "USD";
-    const SYMBOL: &'static str = "$";
-    const RATIO: u8 = 100;
-}
-
-#[derive(Debug, Clone, Copy)]
-pub struct GBP;
-
-impl Currency for GBP {
-    const CODE: &'static str = "GBP";
-    const SYMBOL: &'static str = "£";
-    const RATIO: u8 = 100;
-}
+use std::io::prelude::*;
+use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
+use std::thread::JoinHandle;
+use std::time::Duration;
 
 fn main() {
-    let a = Amount::new(100, USD);
-    let z = Amount::new(100, USD);
+    let shutdown = Arc::new(Mutex::new(false));
 
-    let b = Amount::new(88, GBP);
+    let t1 = std::thread::Builder::new()
+        .name("listener".to_owned())
+        .spawn({
+            let shutdown = Arc::clone(&shutdown);
+            move || {
+                println!("Starting server...");
+                let listener = TcpListener::bind("0.0.0.0:9999").unwrap();
+                println!("Starting listener");
 
-    println!("Amount of a={:#}", a);
-    println!("Amount of b={}", b);
+                let mut i = 0;
+                while !*shutdown.lock().unwrap() {
+                    i += 1;
+                    let (stream, socket) = listener.accept().unwrap();
+                    println!("Received connection...from {:?}", socket);
+                    handle_connection(i, stream, socket);
+                }
+            }
+        })
+        .unwrap();
 
-    println!("Amount of a+z={}", a + z);
-    println!("Amount of a+b={}", a + b);
+    let t2 = std::thread::spawn({
+        let shutdown = Arc::clone(&shutdown);
+        move || {
+            while !*shutdown.lock().unwrap() {
+                println!("Hello from the second thread");
+                std::thread::sleep(Duration::from_secs(2));
+            }
+        }
+    });
+
+    let stream = t1.join().expect("thread is supposed to finish");
+    println!("Thread finished");
+    // set shutdown....
+    {
+        let mut guard = shutdown.lock().unwrap();
+        *guard = true;
+        // lock dropped.
+    }
+    println!("Shutdown initiated");
+
+    loop {
+        println!("Should have stopped....");
+        std::thread::sleep(Duration::from_secs(5));
+    }
+}
+
+fn handle_connection(i: usize, mut stream: TcpStream, addr: SocketAddr) -> JoinHandle<()> {
+    std::thread::Builder::new()
+        .name(format!("t{}", i))
+        .spawn(move || {
+            println!("Handling connection....{:?}", addr);
+            loop {
+                stream.write(b"Hello from the server\n").unwrap();
+                std::thread::sleep(Duration::from_secs(1));
+            }
+        })
+        .unwrap()
 }
